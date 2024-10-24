bf350323dce24ea628c038713981306731ec6d23
wilsonsal <40734746+wilsonsal@users.noreply.github.com>
2024-10-24 07:20:35 -0400

    cambios

--- BD_mysql.js
+++ BD_mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost', // Cambia esto según tu configuración
-  user: 'root',      // Cambia esto según tu configuración
-  password: '',      // Cambia esto según tu configuración
-  database: 'tiendasalinas_db',  // Cambia esto según tu configuración
+  host: 'MYSQL8003.site4now.net', // Cambia esto según tu configuración
+  user: 'aae0e7_prueba',      // Cambia esto según tu configuración
+  password: 'Salinas3443',      // Cambia esto según tu configuración
+  database: 'db_aae0e7_prueba',  // Cambia esto según tu configuración
   connectionLimit: 10 // Número máximo de conexiones en el pool
 });
 
